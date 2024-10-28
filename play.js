dcc88309fb3aa3a421a6fed34bfe3bac30f43e09
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-10-27 19:57:21 -0600

    input.js and play.js

--- play.js
+++ play.js
@@ -1,23 +1,6 @@
 const connect = require('./client.js');
-const handleUserInput = function (key) {
-  console.log(key);
-  if (key === "\u0003") {
-    process.exit();
-  }
-}
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
-  stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
-  stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
-  stdin.resume(); // resume stdin so the program can listen for input
-  stdin.on("data", handleUserInput);
-  return stdin;   // return the stdin object so we can use it elsewhere in the program
-};
+const setupInput = require('./input.js');
 
 console.log("Connecting ...");
 connect();
 setupInput();
-
-
-
